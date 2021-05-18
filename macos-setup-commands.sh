8d79bea42b7224663cecd834c15d617dccdabb42
Antonio Guerriere <antonioguerriere@gmail.com>
2021-05-18 17:14:49 +0100

    added mac settings

--- macos-setup-commands.sh
+++ macos-setup-commands.sh
@@ -31,13 +31,14 @@ brew install --cask flux
 # add volume controller to menu bar: sys preferences, sound add tick show volume to menu bar
 # enable hot corners, to enable show desktop at the bottom right
 # enable tab to move focus between controls: sys pref, shortcuts, add tick to Use keyboard navigation to move focus between controls
-
+# ----------------
 # finder settings:
 # open finder(any window)
 # in the menu bar on top click finder, preferences, sidebar. Hide tags, and enable folders you prefer.
 # then go to advanced and add tick to show all filename extensions
-
+# -----------------
 # Open sys preferences: dock & menu bar: minimize windows using Scale Effect(not genie),tick the following:
 # double click a window's title bar to zoom, minimise windows into application icon, animate opening application, automatically hide and show the dock, show indicators for open applications
 # DESELECT show recent applications in Dock, and automatically hide and show the menu bar
 # set dock size to max
+# -----------------
